c723b7e3afcb565cde4b24bde4733c49a7b7484d
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-08-17 01:15:44 +0100

    Clippy, Error handling, generics, std library types, traits

--- rustlings/exercises/standard_library_types/box1.rs
+++ rustlings/exercises/standard_library_types/box1.rs
@@ -15,12 +15,9 @@
 // Note: the tests should not be changed
 //
 // Execute `rustlings hint box1` for hints :)
-
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -33,11 +30,11 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    unimplemented!()
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    unimplemented!()
+    List::Cons(0, Box::new(List::Nil))
 }
 
 #[cfg(test)]
