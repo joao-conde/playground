5b77ea89944a6ef75c9531ab42f3e704e20bd826
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-05-27 16:06:01 +0100

    refactor: internal error

--- apps/todo-actix/src/error.rs
+++ apps/todo-actix/src/error.rs
@@ -2,22 +2,41 @@ use actix_web::{body::BoxBody, error::BlockingError, http::StatusCode, HttpRespo
 use std::fmt::Display;
 
 #[derive(Debug)]
-pub enum ApiError {
-    NotFound,
-    Internal(String),
+pub enum InternalError {
+    Sql(rusqlite::Error),
+    ConnectionPool(r2d2::Error),
+    Actix(actix_web::Error),
+}
+
+impl From<rusqlite::Error> for InternalError {
+    fn from(err: rusqlite::Error) -> Self {
+        Self::Sql(err)
+    }
+}
+
+impl From<BlockingError> for InternalError {
+    fn from(err: BlockingError) -> Self {
+        Self::Actix(err.into())
+    }
+}
+
+impl From<r2d2::Error> for InternalError {
+    fn from(err: r2d2::Error) -> Self {
+        Self::ConnectionPool(err)
+    }
 }
 
 #[derive(Debug)]
-pub enum InternalError {
+pub enum ApiError {
     NotFound,
-    Internal(String),
+    Internal,
 }
 
 impl From<InternalError> for ApiError {
     fn from(err: InternalError) -> Self {
         match err {
-            InternalError::NotFound => Self::NotFound,
-            InternalError::Internal(msg) => Self::Internal(msg),
+            InternalError::Sql(rusqlite::Error::QueryReturnedNoRows) => Self::NotFound,
+            _ => Self::Internal,
         }
     }
 }
@@ -26,7 +45,7 @@ impl Display for ApiError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             ApiError::NotFound => write!(f, "Not Found Error"),
-            ApiError::Internal(msg) => write!(f, "Internal Server Error: {msg}"),
+            ApiError::Internal => write!(f, "Internal Server Error"),
         }
     }
 }
@@ -35,32 +54,11 @@ impl actix_web::ResponseError for ApiError {
     fn status_code(&self) -> StatusCode {
         match self {
             ApiError::NotFound => StatusCode::NOT_FOUND,
-            ApiError::Internal(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ApiError::Internal => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 
     fn error_response(&self) -> HttpResponse<BoxBody> {
         HttpResponse::build(self.status_code()).body(self.to_string())
-    }
-}
-
-impl From<rusqlite::Error> for InternalError {
-    fn from(err: rusqlite::Error) -> Self {
-        match err {
-            rusqlite::Error::QueryReturnedNoRows => Self::NotFound,
-            _ => Self::Internal(err.to_string()),
-        }
-    }
-}
-
-impl From<BlockingError> for InternalError {
-    fn from(err: BlockingError) -> Self {
-        Self::Internal(err.to_string())
-    }
-}
-
-impl From<r2d2::Error> for InternalError {
-    fn from(err: r2d2::Error) -> Self {
-        Self::Internal(err.to_string())
     }
 }
