238270f8c82080665af8adade49f4e6879b9e49d
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-05-27 12:50:56 +0100

    feat: API error definition

--- apps/todo-actix/src/main.rs
+++ apps/todo-actix/src/main.rs
@@ -1,38 +1,48 @@
 mod db;
 
-use actix_web::{delete, get, post, put, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{delete, get, post, put, web, App, HttpResponse, HttpServer};
+use db::ApiError;
 use r2d2_sqlite::SqliteConnectionManager;
 
 const HOST: &str = "127.0.0.1";
 const PORT: u16 = 8080;
 
 #[get("/todos")]
-async fn list_todos(pool: web::Data<db::Pool>) -> impl Responder {
-    let todos = db::list_todos(&pool).await.unwrap();
-    HttpResponse::Ok().json(todos)
+async fn list_todos(pool: web::Data<db::Pool>) -> Result<HttpResponse, ApiError> {
+    let todos = db::list_todos(&pool).await?;
+    Ok(HttpResponse::Ok().json(todos))
 }
 
 #[post("/todos")]
-async fn create_todo(pool: web::Data<db::Pool>, todo: web::Json<db::CreateTodo>) -> impl Responder {
-    let created = db::create_todo(&pool, todo.into_inner()).await.unwrap();
-    HttpResponse::Ok().json(created)
+async fn create_todo(
+    pool: web::Data<db::Pool>,
+    todo: web::Json<db::CreateTodo>,
+) -> Result<HttpResponse, ApiError> {
+    let created = db::create_todo(&pool, todo.into_inner()).await?;
+    Ok(HttpResponse::Ok().json(created))
 }
 
 #[get("/todos/{id}")]
-async fn get_todo(pool: web::Data<db::Pool>, id: web::Path<usize>) -> impl Responder {
-    let todo = db::get_todo(&pool, *id).await.unwrap();
-    HttpResponse::Ok().json(todo)
+async fn get_todo(
+    pool: web::Data<db::Pool>,
+    id: web::Path<usize>,
+) -> Result<HttpResponse, ApiError> {
+    let todo = db::get_todo(&pool, *id).await?;
+    Ok(HttpResponse::Ok().json(todo))
 }
 
 #[put("/todos")]
-async fn update_todo(todo: web::Json<db::Todo>) -> impl Responder {
-    HttpResponse::Ok().json(todo)
+async fn update_todo(todo: web::Json<db::Todo>) -> Result<HttpResponse, ApiError> {
+    Ok(HttpResponse::Ok().json(todo))
 }
 
 #[delete("/todos/{id}")]
-async fn delete_todo(pool: web::Data<db::Pool>, id: web::Path<usize>) -> impl Responder {
-    let deleted = db::delete_todo(&pool, *id).await.unwrap();
-    HttpResponse::Ok().json(deleted)
+async fn delete_todo(
+    pool: web::Data<db::Pool>,
+    id: web::Path<usize>,
+) -> Result<HttpResponse, ApiError> {
+    let deleted = db::delete_todo(&pool, *id).await?;
+    Ok(HttpResponse::Ok().json(deleted))
 }
 
 #[actix_web::main]
