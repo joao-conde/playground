869c7b7d1b02cce19aa91d84a81883a2a360a2c3
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-10-15 21:07:58 +0100

    refactor: mini tokio modules

---
+++ rust/tokio_tutorial/mini-tokio/src/delay.rs
@@ -0,0 +1,37 @@
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::thread;
+use std::time::Instant;
+
+pub struct Delay {
+    pub when: Instant,
+}
+
+impl Future for Delay {
+    type Output = &'static str;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<&'static str> {
+        if Instant::now() >= self.when {
+            println!("Hello world");
+            Poll::Ready("done")
+        } else {
+            // Get a handle to the waker for the current task
+            let waker = cx.waker().clone();
+            let when = self.when;
+
+            // Spawn a timer thread.
+            thread::spawn(move || {
+                let now = Instant::now();
+
+                if now < when {
+                    thread::sleep(when - now);
+                }
+
+                waker.wake();
+            });
+
+            Poll::Pending
+        }
+    }
+}
